8dc937f929fc5462214b9775212b71711d4fb090
M.P.Ross <mpross2@uw.edu>
2018-10-04 09:57:49 -0700

    Added almost finished homework 0.

---
+++ hw0/hw0_9a.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+supMax=0.0025
+n=int(1.0/(2.0*supMax)**2)
+Z=np.random.randn(n)
+
+plt.step(sorted(Z),np.arange(1,n+1)/float(n))
+plt.xlabel("Observations")
+plt.ylabel("Probability")
+plt.show()
+
+
